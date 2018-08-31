dfd8544ac5943b0d41c126b66ea8647af33017a0
Summertime <Summertime@users.noreply.github.com>
2018-09-01 06:44:15 +0930

    Expanding flags to their -- form

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@ set -o nounset
 set -o pipefail
 set -o errexit
 
-rm -r build 2>/dev/null && :
-mkdir -p build
-cp src/* build/
+rm --recursive build 2>/dev/null && :
+echo 'beginning build'
+mkdir --verbose --parents build
+cp --verbose --no-target-directory --recursive src build
