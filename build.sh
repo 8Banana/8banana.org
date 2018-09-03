068db885d9f0cfec452e966325550866063c02a6
Summertime <Summertime@users.noreply.github.com>
2018-09-04 03:43:53 +0930

    "Improving" build script 1. bash to (da)sh 2. allowing executable files to be "built"

--- build.sh
+++ build.sh
@@ -1,9 +1,33 @@
-#!/bin/bash
+#!/bin/sh
 set -o nounset
 set -o pipefail
 set -o errexit
 
 rm --recursive build 2>/dev/null && :
 echo 'beginning build'
-mkdir --verbose --parents build
-cp --verbose --no-target-directory --recursive src build
+
+builder () {
+    local -
+    set -o nounset
+    set -o errexit
+    local source_dir="${1%/}" ; shift
+    local destination_dir="${1%/}" ; shift
+    for source in "$@"; do
+        [ ! -e "$source" ] && continue
+        destination="$destination_dir${source#$source_dir}"
+
+        if [ -d "$source" ]; then
+            mkdir "$destination"
+            builder "$source_dir" "$destination_dir" "$source"/*
+
+        elif [ -x "$source" ]; then
+            "$source" > "$destination"
+
+        elif [ -f "$source" ]; then
+            cat "$source" > "$destination"
+
+        else ERROR; fi
+    done
+}
+
+builder src/ build/ src/
